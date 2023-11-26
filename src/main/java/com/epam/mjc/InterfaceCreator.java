1034c58abedcc7dc23ad719e672441995c755a3b
antonio481 <adanilovx@gmail.com>
2023-11-26 13:57:42 +0400

    DecisionCommut

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,17 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return (List<Integer>arg)->{
+            ArrayList<Integer>result=new ArrayList<>();
+            for (Integer integer : arg) {
+                result.add(integer / divider);
+            }
+        return result;
+        };
     }
 }
